f27a538ac5e38b23c0ebfb281c0edd1205ae40d4
Alex Hockley <hocklealex>
2018-12-29 17:31:24 +1300

    Slideshow now works properly

--- js/hiamo.js
+++ js/hiamo.js
@@ -1,45 +1,40 @@
-var slideIndex = 0;
-var myTimer = setInterval(function() {
-    showSlides(slideIndex);
-}, 7000);
+var slideIndex = 1;
+// Display the first slide on startup
+showSlides(slideIndex);
 
-//showSlides(slideIndex);
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
 
-var plus = function plusSlides(n) {
+var myTimer = setInterval(nextSlide, 5000);
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
     clearInterval(myTimer);
-    myTimer = setInterval(function() {
-        console.log("********PLUS*******");
-        console.log(n);
-        console.log(slideIndex + n);
-        showSlides(slideIndex += n);
-    }, 5000);
-};
+    myTimer = setInterval(nextSlide, 5000);
+}
 
 var current = function currentSlide(n) {
-    clearInterval(myTimer);
-    myTimer = setInterval(function() {
-        showSlides(slideIndex = n);
-    }, 5000);
+    showSlides(slideIndex = n);
 };
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n >= slides.length) {slideIndex = 0}    
-    if (n < 0) {slideIndex = slides.length-1}
+    // var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
+       slides[i].style.display = "none";
     }
-    slideIndex++;  
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    console.log("********SHOW*******");
-    console.log(slideIndex-1);
-    dots[slideIndex-1].className += " active";
-    // setTimeout(function() {
-    //     showSlides(slideIndex);
-    // }, 5000); // Change image every 5 seconds
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    slides[slideIndex-1].style.display = "block";
+    // dots[slideIndex-1].className += " active";
 }
