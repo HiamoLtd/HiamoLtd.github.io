43c1738e0ed93740a434930d913b21beb5743a1e
unknown <khanshai@ecs.vuw.ac.nz>
2020-04-10 16:14:59 +1200

    Created shared column class for titled information sections

---
+++ hiamo-site/src/components/shared/titledcol.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Import Bootstrap components
+import Col from "react-bootstrap/Col";
+
+import styles from './titledcol.module.css'
+
+const TitledCol = ({ header, children, width=4, headerBgColor, headerColor, paddingSide=0 }) => (
+  <Col md={width} className={styles.container}>
+    <div className={styles.headerWrapper} style={{backgroundColor: `var(--${headerBgColor || 'color-trim'})`}}>
+      <h3
+        className={styles.header} 
+        style={{
+          color: `var(--${headerColor || 'color-text-light'})`,
+          paddingLeft:`${paddingSide}`,
+          paddingRight:`${paddingSide}`
+        }}
+      >
+        {header}
+      </h3>
+    </div>
+    <div
+      className={styles.contentWrapper}
+      style={{
+        paddingLeft:`${paddingSide}`,
+        paddingRight:`${paddingSide}`
+      }}
+    >
+      {children}
+    </div>
+  </Col>
+);
+
+TitledCol.propTypes = {
+  header: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  width: PropTypes.number,
+  headerBgColor: PropTypes.string,
+  headerColor: PropTypes.string,
+  paddingSide: PropTypes.number,
+}
+
+export default TitledCol;
--- hiamo-site/src/components/team/team.js
+++ hiamo-site/src/components/team/team.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// Import Bootstrap components
-import Col from "react-bootstrap/Col";
-
+import { TitledCol } from "../shared";
 import styles from './team.module.css'
 
 // TODO do we want images?
 const Team = ({ name, job, email, linkedIn, width, nameBgColor, nameColor }) => (
-  <Col md={width || 4} className={styles.container}>
-    <div className={styles.headerWrapper} style={{backgroundColor: `var(--${nameBgColor || 'color-trim'})`}}>
-      <h3 className={styles.header} style={{color: `var(--${nameColor || 'color-text-light'})`}}>
-        {name}
-      </h3>
-    </div>
-    <div className={styles.contentWrapper}>
-      <h4 className={styles.job}>
-        {job}
-      </h4>
-      {email &&
-        <a href={`mailto:${email}`} className={styles.email}>
-          Email
-        </a>
-      }
-      {linkedIn &&
-        <a href={linkedIn} className={styles.link}>
-          LinkedIn
-        </a>
-      }
-    </div>
-  </Col>
+  <TitledCol
+    width={width}
+    header={name}
+    headerBgColor={nameBgColor}
+    headerColor={nameColor}
+    paddingSide="30px"
+  >
+    <h4 className={styles.job}>
+      {job}
+    </h4>
+    {email &&
+      <a href={`mailto:${email}`} className={styles.email}>
+        Email
+      </a>
+    }
+    {linkedIn &&
+      <a href={linkedIn} className={styles.link}>
+        LinkedIn
+      </a>
+    }
+  </TitledCol>
 );
 
 Team.propTypes = {
