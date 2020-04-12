b3f48dc788270f846b15edd27379312585aaf2e9
unknown <khanshai@ecs.vuw.ac.nz>
2020-04-12 19:43:32 +1200

    Section can now be set to be the main header on a page

--- hiamo-site/src/components/sections/section.js
+++ hiamo-site/src/components/sections/section.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 
 import styles from './section.module.css';
 
-const Section = ({ children, hasLine = true, title }) => (
+const Section = ({ children, hasLine = true, title, mainHeader=false }) => (
   <div className={`${styles.container} ${hasLine ? styles.afterLine : ''}`}>
-    {title &&
+    {(title && !mainHeader) &&
       <h2 className={styles.title}>{title}</h2>
+    }
+    {(title && mainHeader) &&
+      <h1 className={styles.title}>{title}</h1>
     }
     {children}
   </div>
