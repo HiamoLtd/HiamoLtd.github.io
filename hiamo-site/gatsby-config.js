59b0cde0a7ed981ddb9e4294a1b86a2daa636d7a
unknown <khanshai@ecs.vuw.ac.nz>
2020-04-12 19:43:46 +1200

    Adjusted page names in config

--- hiamo-site/gatsby-config.js
+++ hiamo-site/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Hiamo - Augmented Reality for Historic Sites`,
+    title: `Hiamo`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `Hiamo Ltd.`,
   },
