56d57dc1a41206551e29dcd8323a31e895578254
unknown <khanshai@ecs.vuw.ac.nz>
2020-08-18 21:23:50 +1200

    Reverted banner to correct approach.

--- hiamo-site/src/components/banner/banner.js
+++ hiamo-site/src/components/banner/banner.js
@@ -1,43 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react"
+import PropTypes from "prop-types"
 
 // Import Bootstrap tools
-import { Row, Col, Image } from 'react-bootstrap';
-import { Section } from '../index.js';
+import { Row, Col, Image } from "react-bootstrap"
+import { Section } from "../index.js"
 
-import styles from './banner.module.css';
+import styles from "./banner.module.css"
 
-const Banner = ({
-  title,
-  subtitle,
-  imageRef
-}) => (
+const Banner = ({ title, subtitle, imageRef }) => (
   <Section hasLine={false}>
     <Row className={styles.container}>
-      <Col md={imageRef ? 8 : 12} className={styles.titleWrapper}>
-        <h1 className={imageRef ? styles.imageTitle : styles.title}>
-          {title}
-        </h1>
-        <h2 className={imageRef ? styles.imageSubtitle : styles.subtitle}>
-          {subtitle}
-        </h2>
+      <Col md={8} className={styles.titleWrapper}>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
       </Col>
-      { imageRef &&
+      {imageRef && (
         <Col md={4} className={styles.imageWrapper}>
-          <Image
-            src={imageRef}
-            className={styles.image}
-          />
+          <Image src={imageRef} className={styles.image} />
         </Col>
-      }
+      )}
     </Row>
   </Section>
-);
+)
 
 Banner.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
-  imageRef: PropTypes.string.isRequired
+  imageRef: PropTypes.string.isRequired,
 }
 
-export default Banner;
+export default Banner
