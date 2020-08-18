1718ff2ac196232aae970036cf20bfd4220614b1
unknown <khanshai@ecs.vuw.ac.nz>
2020-08-18 20:38:49 +1200

    Further linting fixes

--- hiamo-site/gatsby-config.js
+++ hiamo-site/gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    'gatsby-plugin-eslint',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
