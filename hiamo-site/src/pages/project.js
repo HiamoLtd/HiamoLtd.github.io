914857af273047180c391c5d5f3f85649cb9877e
unknown <khanshai@ecs.vuw.ac.nz>
2020-08-18 20:03:36 +1200

    Moved project location

--- hiamo-site/src/pages/project.js
+++ hiamo-site/src/pages/project.js
@@ -3,33 +3,47 @@ import React from "react";
 // Import bootstrap components
 import { Row, Col } from "react-bootstrap";
 
-import { Screen, Section, ProjectBox } from '../../components';
-import { SEO } from '../../components/shared';
+import { Banner, Screen, Section, ProjectBox } from '../components';
+import { SEO } from '../components/shared';
 
 import styles from './project.module.css';
 
+const title = "Aotearoa's Coastal Defence";
+const subtitle = "Recreating objects digitally & returning context to heritage landmarks.";
+
 const Project = () => (
   <Screen>
-    <SEO title="XXXXXXXXX" />
-    <Section title="XXXXXXXXX" mainHeader>
+    <SEO title={title} />
+    <Banner
+      title={title}
+      subtitle={subtitle}
+    />
+    <Section>
       {/* Title section */}
-      <Row className={styles.titleRow}>
+      {/* <Row className={styles.titleRow}>
         <Col md={12}>
           <p className={styles.date}>
             dd/mm/yy
           </p>
         </Col>
-      </Row>
+      </Row> */}
       {/* Main content section */}
       <Row className={styles.endRow}>
         {/* Left side text */}
         <Col md={6} className={styles.mainCol}>
+          <p>
+          New Zealand is covered in coastal defence sites, but many of them lack the naval guns that they were built for.
+
+We bring these objects back digitally, returning context to the site and enabling lifelong learning through interaction with the models, all without the need to construct or maintain replicas.
+
+To further engage with this history and the public, we bring these applications to the Wrights Hill Fortress open days in Wellington.
+          </p>
         </Col>
         {/* Right side text */}
         <Col md={6} className={styles.mainCol}>
         </Col>
       </Row>
-    </Section>
+    </Section> 
     <Section title="OTHER PROJECTS">
       <Row className={styles.otherProjectsWrapper}>
         <ProjectBox
