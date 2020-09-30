07669e65d9cfae405e4ef90ed1494ef34ccf5fd3
unknown <khanshai@ecs.vuw.ac.nz>
2020-09-30 23:44:19 +1300

    Added animated hamburger for mobile

--- hiamo-site/src/components/layout/header/header.js
+++ hiamo-site/src/components/layout/header/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,7 +12,7 @@ function getLink(link, slug) {
   // Check what page we are on.
   const currPage = typeof window !== 'undefined' ? window.location.pathname : '';
   // Currently, all header pages are on the home page. If we aren't at home page, link us there
-  return (currPage === '/' || slug === '/') ? `/#${link}` : `${slug}/${link}`;
+  return currPage === '/' || slug === '/' ? `/#${link}` : `${slug}/${link}`;
 }
 
 const NavLink = ({ content, link }) => (
@@ -21,36 +21,54 @@ const NavLink = ({ content, link }) => (
   </Link>
 );
 
-const Header = () => (
-  <div className={styles.container}>
-    <Navbar className={`content align-items-end ${styles.nav}`} expand="lg" sticky="top">
-      {/* Brand sign */}
-      <Link to="/">
-        <Navbar.Brand className={styles.brandWrapper}>
-          <img
-            src={require('../../../images/icons/logo_name.png')}
-            height="60"
-            className={`${styles.brand} d-inline-block align-top`}
-            alt="Hiamo navbar logo"
-          />
-        </Navbar.Brand>
-      </Link>
-      {/* Collapse controls */}
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse
-        id="basic-navbar-nav"
-        className={`${styles.linkWrapper} align-items-end justify-content-end`}
-      >
-        {/* Links */}
-        <NavLink content="HOME" link="/" />
-        <NavLink content="ABOUT" link={getLink('About', '/')} />
-        <NavLink content="PROJECTS" link={getLink('Projects', '/')} />
-        <NavLink content="TEAM" link={getLink('Team', '/')} />
-        <NavLink content="BLOG" link={getLink('Blog', '/')} />
-      </Navbar.Collapse>
-    </Navbar>
-  </div>
-);
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={styles.container}>
+      <Navbar className={`content align-items-end ${styles.nav}`} expand="lg" sticky="top">
+        {/* Brand sign */}
+        <Link to="/">
+          <Navbar.Brand>
+            <img
+              src={require('../../../images/icons/logo_name.png')}
+              className={`${styles.brand} d-inline-block align-top`}
+              alt="Hiamo navbar logo"
+            />
+          </Navbar.Brand>
+        </Link>
+        {/* Collapse controls */}
+        <Navbar.Toggle
+          className={`${styles.hamburgerWrapper}`}
+          type="button"
+          data-toggle="collapse"
+          aria-controls="Header navbar toggle"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setOpen(!open)}
+        >
+          <div className={`${styles.hamburger} ${open && styles.open}`}>
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+        </Navbar.Toggle>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className={`${styles.linkWrapper} align-items-end justify-content-end`}
+        >
+          {/* Links */}
+          <NavLink content="HOME" link="/" />
+          <NavLink content="ABOUT" link={getLink('About', '/')} />
+          <NavLink content="PROJECTS" link={getLink('Projects', '/')} />
+          <NavLink content="TEAM" link={getLink('Team', '/')} />
+          <NavLink content="BLOG" link={getLink('Blog', '/')} />
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 NavLink.propTypes = {
   content: PropTypes.string.isRequired,
