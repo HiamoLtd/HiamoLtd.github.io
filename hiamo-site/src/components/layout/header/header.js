437e16d3e7b53120cc953ee967d26911896d4061
unknown <khanshai@ecs.vuw.ac.nz>
2020-10-01 22:42:12 +1300

    Brand closes header. Header now does not bounce page on expanding.

--- hiamo-site/src/components/layout/header/header.js
+++ hiamo-site/src/components/layout/header/header.js
@@ -16,9 +16,11 @@ function getLink(link, slug) {
 }
 
 const NavLink = ({ content, link, toggleNav }) => (
-  <Link to={link} className={`${styles.link} nav-link`} onClick={toggleNav}>
-    {content}
-  </Link>
+  <div className={`${styles.linkWrapper}  nav-link`}>
+    <Link to={link} className={styles.link} onClick={toggleNav}>
+      {content}
+    </Link>
+  </div>
 );
 
 const Header = () => {
@@ -32,55 +34,50 @@ const Header = () => {
 
   return (
     <div className={styles.container}>
-      <Navbar
-        className={`content align-items-end ${styles.nav}`}
-        expand="lg"
-        sticky="top"
-        expanded={open}
-      >
-        {/* Brand sign */}
-        <Link to="/">
-          <Navbar.Brand>
-            <img
-              src={require('../../../images/icons/logo_name.png')}
-              className={`${styles.brand} d-inline-block align-top`}
-              alt="Hiamo navbar logo"
-            />
-          </Navbar.Brand>
-        </Link>
-        {/* Collapse controls */}
-        <Navbar.Toggle
-          className={`${styles.hamburgerWrapper}`}
-          type="button"
-          data-toggle="collapse"
-          aria-controls="Header navbar toggle"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={() => setOpen(open ? false : 'expanded')}
+      <div className={styles.navWrapper}>
+        <Navbar
+          className={`content align-items-end ${styles.nav}`}
+          expand="lg"
+          sticky="top"
+          expanded={open}
         >
-          <div className={`${styles.hamburger} ${!!open && styles.open}`}>
-            <span />
-            <span />
-            <span />
-            <span />
-          </div>
-        </Navbar.Toggle>
-        <Navbar.Collapse
-          id="basic-navbar-nav"
-          className={`${styles.linkWrapper} align-items-end justify-content-end`}
-          onSelect={() => {
-            console.log('tEst:', open);
-            setOpen(false);
-          }}
-        >
-          {/* Links */}
-          <NavLink content="HOME" link="/" toggleNav={onSelect} />
-          <NavLink content="ABOUT" link={getLink('About', '/')} toggleNav={onSelect} />
-          <NavLink content="PROJECTS" link={getLink('Projects', '/')} toggleNav={onSelect} />
-          <NavLink content="TEAM" link={getLink('Team', '/')} toggleNav={onSelect} />
-          <NavLink content="BLOG" link={getLink('Blog', '/')} toggleNav={onSelect} />
-        </Navbar.Collapse>
-      </Navbar>
+          {/* Brand sign */}
+          <Link to="/" onClick={onSelect}>
+            <Navbar.Brand>
+              <img
+                src={require('../../../images/icons/logo_name.png')}
+                className={`${styles.brand} d-inline-block align-top`}
+                alt="Hiamo navbar logo"
+              />
+            </Navbar.Brand>
+          </Link>
+          {/* Collapse controls */}
+          <Navbar.Toggle
+            className={`${styles.hamburgerWrapper}`}
+            type="button"
+            data-toggle="collapse"
+            aria-controls="Header navbar toggle"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setOpen(open ? false : 'expanded')}
+          >
+            <div className={`${styles.hamburger} ${!!open && styles.open}`}>
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav" className={`${styles.linkList} align-items-end justify-content-end`}>
+            {/* Links */}
+            <NavLink content="HOME" link="/" toggleNav={onSelect} />
+            <NavLink content="ABOUT" link={getLink('About', '/')} toggleNav={onSelect} />
+            <NavLink content="PROJECTS" link={getLink('Projects', '/')} toggleNav={onSelect} />
+            <NavLink content="TEAM" link={getLink('Team', '/')} toggleNav={onSelect} />
+            <NavLink content="BLOG" link={getLink('Blog', '/')} toggleNav={onSelect} />
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     </div>
   );
 };
