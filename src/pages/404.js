440db689eafa61cd098d95e021536891f44b73b2
unknown <khanshai@ecs.vuw.ac.nz>
2020-10-13 22:36:04 +1300

    404 page now formatted correctly, for the time being

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-import { Screen, Section } from '../components';
+import { Screen, Banner } from '../components';
 import { SEO } from '../components/shared';
+
+import styles from './404.module.css';
 
 const NotFoundPage = () => (
   <Screen>
     <SEO title="404: Not found" />
-    <Section title="Page Not Found">
-      <p>We commend your explorative nature, but it seems there is nothing here!</p>
-    </Section>
+    <div className={styles.container}>
+      <Banner
+        title="PAGE NOT FOUND"
+        subtitle="We commend your explorative nature, but it seems there is nothing here!"
+        imageRef={require('../images/default.jpg')}
+      />
+    </div>
   </Screen>
 );
 
