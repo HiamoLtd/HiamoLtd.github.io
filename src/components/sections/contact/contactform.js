1d3430d45455e9299fe038314f5cea6fe0c11d20
unknown <khanshai@ecs.vuw.ac.nz>
2020-10-14 22:37:52 +1300

    Contact form now submit via js, and has a popup

--- src/components/sections/contact/contactform.js
+++ src/components/sections/contact/contactform.js
@@ -1,57 +1,94 @@
+/* eslint-disable no-alert */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-// TODO fix above
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './contact.module.css';
 
-// TODO need a confirmation for the user. Maybe contact form shouldnt reset page?
-const ContactForm = () => (
-  // Attach Netlify form handling to form
-  <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-    <input type="hidden" name="bot-field" />
-    {/* A row for name and organisation */}
-    <div className={styles.inputWrapper}>
-      <label htmlFor="name">
-        Name
-      </label>
-      <input type="text" name="name" id="name" />
-    </div>
-    <div className={styles.inputWrapper}>
-      <label htmlFor="company" className={styles.companyLong}>
-        Company / Organisation
-      </label>
-      <label htmlFor="company" className={styles.companyShort}>
-        Company
-      </label>
-      <input type="text" name="company" id="company" />
-    </div>
-    {/* A row for email */}
-    <div className={styles.inputWrapper}>
-      <label htmlFor="email">
-        Email
-      </label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="This one's a must have."
-        required
-      />
-    </div>
-    {/* A row for content */}
-    <div className={styles.inputWrapper}>
-      <label htmlFor="message">
-        Message
-      </label>
-      <textarea name="message" className={styles.message} />
-    </div>
-    {/* A row for a submission button */}
-    <div className={styles.inputWrapper}>
-      <div className={styles.submit}>
-        <input className={styles.submitBtn} type="submit" id="contact-send" value="Send" />
+const ContactForm = () => {
+  // Track the form values in state
+  const [values, setValues] = useState({
+    name: '',
+    company: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = e => setValues({ ...values, [e.target.name]: e.target.value });
+
+  // Encode the data so the form can submit it as text
+  const encode = data => Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+
+  // Submit the form to netlify and inform the user
+  const handleSubmit = (e) => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...values }),
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  return (
+    // Attach Netlify form handling to form
+    <form onSubmit={handleSubmit} method="post" netlify-honeypot="bot-field" data-netlify="true">
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      {/* A row for name and organisation */}
+      <div className={styles.inputWrapper}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" onChange={handleChange} value={values.name} />
       </div>
-    </div>
-  </form>
-);
+      <div className={styles.inputWrapper}>
+        <label htmlFor="company" className={styles.companyLong}>
+          Company / Organisation
+        </label>
+        <label htmlFor="company" className={styles.companyShort}>
+          Company
+        </label>
+        <input
+          type="text"
+          name="company"
+          id="company"
+          onChange={handleChange}
+          value={values.company}
+        />
+      </div>
+      {/* A row for email */}
+      <div className={styles.inputWrapper}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={handleChange}
+          value={values.email}
+          required
+          placeholder="This one's a must have."
+        />
+      </div>
+      {/* A row for content */}
+      <div className={styles.inputWrapper}>
+        <label htmlFor="message">Message</label>
+        <textarea
+          name="message"
+          className={styles.message}
+          onChange={handleChange}
+          value={values.message}
+        />
+      </div>
+      {/* A row for a submission button */}
+      <div className={styles.inputWrapper}>
+        <div className={styles.submit}>
+          <input className={styles.submitBtn} type="submit" id="contact-send" value="Send" />
+        </div>
+      </div>
+    </form>
+  );
+};
 
 export default ContactForm;
